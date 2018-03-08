c0a623d4a1c86ae3624e6abbe0a53f03423d1a2c
severnsc <severnsc@gmail.com>
2018-03-08 18:40:53 -0500

    remove logic that is better suited for boundaries

--- interactors.js
+++ interactors.js
@@ -7,51 +7,31 @@ const getQuestions = async fetch => {
   return parsedResponse.results
 }
 
-const createNewGame = (questions, save) => {
+const newGame = (questions, save) => {
   const questionEntities = questions.map(question => 
     entities.createQuestion(question.category, question.text, question.correctAnswer, question.incorrectAnswers)
   )
   const newGame = entities.createGame(questionEntities)
-  const condensedQuestions = newGame.questions.map(question => ({
-      id: question.id,
-      category: question.category,
-      text: question.text, 
-      answers: [...question.incorrectAnswers, question.correctAnswer]
-    })
-  )
   save(newGame)
   return {
     id: newGame.id,
-    questions: condensedQuestions,
+    questions: newGame.questions,
+    submittedAnswers: newGame.submittedAnswers,
+    numberCorrect: newGame.numberCorrect,
+    totalAnswered: newGame.totalAnswered,
     complete: newGame.complete
   }
 }
 
-const submitAnswer = (gameId, questionId, answer, find) => {
-  const currentGame = find(gameId)
-  currentGame.submitAnswer({questionId, answer})
-  return {
-    id: currentGame.id,
-    questions: currentGame.questions,
-    submittedAnswers: currentGame.submittedAnswers,
-    complete: currentGame.complete
-  }
-}
-
-const getGameWithScore = (gameId, find) => {
-  const currentGame = find(gameId)
-  return {
-    id: currentGame.id,
-    questions: currentGame.questions,
-    submittedAnswers: currentGame.submittedAnswers,
-    complete: currentGame.complete,
-    numberCorrect: currentGame.numberCorrect,
-    totalAnswered: currentGame.totalAnswered
-  }
+const submitAnswer = (gameId, find, questionId, answer) => {
+  const game = find(gameId)
+  const submittedAnswer = {questionId, answer}
+  game.submitAnswer(submittedAnswer)
+  return game
 }
 
 module.exports = {
+  getQuestions,
   createNewGame,
-  submitAnswer,
-  getGameWithScore
+  submitAnswer
 }
