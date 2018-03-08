700e6acacf6f8229045f691200d0fca853fdb3e5
severnsc <severnsc@gmail.com>
2018-03-08 18:41:14 -0500

    create boundaries

---
+++ boundaries.js
@@ -0,0 +1,32 @@
+const interactors = require('./interactors')
+const fetch = require('node-fetch')
+
+const createNewGame = () => {
+  const questions = interactors.getQuestions(fetch).then(questions)
+  const newGame = interactors.newGame(questions, game => {})
+  const dtoQuestions = newGame.questions.map(question => ({
+      id: question.id,
+      category: question.category,
+      text: question.text, 
+      answers: [...question.incorrectAnswers, question.correctAnswer]
+    })
+  )
+  return Object.assign({}, newGame, {questions: dtoQuestions})
+}
+
+const submitAnswer = (gameId, questionId, answer) => {
+  const game = interactors.submitAnswer(gameId, id => {}, questionId, answer)
+  const dtoQuestions = game.questions.map(question => ({
+      id: question.id,
+      category: question.category,
+      text: question.text, 
+      answers: [...question.incorrectAnswers, question.correctAnswer]
+    })
+  )
+  return Object.assign({}, game, {questions: dtoQuestions})
+}
+
+module.exports = {
+  createNewGame,
+  submitAnswer
+}
