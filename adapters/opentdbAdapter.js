77a0199dd0e6293ef02113207ae0f4431378e73e
severnsc <severnsc@gmail.com>
2018-03-14 11:18:51 -0400

    Refactor questions adapter to create and return questionEntities

--- adapters/opentdbAdapter.js
+++ adapters/opentdbAdapter.js
@@ -1,11 +1,15 @@
 const fetch = require('node-fetch')
+const core = require('../lib')
 
 const fetchQuestions = async () => {
   const url = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
   const response = await fetch(url)
   const json = await response.json()
   const results = json.results
-  return results
+  const questionEntities = results.map(result => {
+    return core.createQuestionEntity(result.category, result.question, result.correct_answer, result.incorrect_answers)
+  })
+  return questionEntities
 }
 
 module.exports = {
