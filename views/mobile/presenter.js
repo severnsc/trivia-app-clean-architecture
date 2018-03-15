a2c4c33cb55f65ee5cdf800c7552d0bab967750d
severnsc <severnsc@gmail.com>
2018-03-15 18:10:33 -0400

    add answerQuestion to presenter

--- views/mobile/presenter.js
+++ views/mobile/presenter.js
@@ -1,26 +1,65 @@
 const compose = require('./compose')
 const globalViewModel = require('../globalViewModel')
 
-const createGame = async () => {
-  const game = await compose.createGame()
-  const currentQuestion = game.questions[0]
+const getGameModel = game => {
+  const questionsIndex = game.answers.length
+  const currentQuestion = game.questions[questionsIndex]
+ 
   const answers = [
     ...currentQuestion.incorrectAnswers,
     currentQuestion.correctAnswer
   ]
-  const currentQuestionNumber = 1
+ 
+  const currentQuestionNumber = questionsIndex + 1
+ 
   const currentQuestionModel = globalViewModel.currentQuestion(
     currentQuestion.category,
     currentQuestion.text,
     answers,
     currentQuestionNumber
   )
+  
   const gameModel = globalViewModel.game(game.id, currentQuestionModel)
   return gameModel
 }
 
-createGame().then(res => console.log(res))
+const createGame = async () => {
+  const game = await compose.createGame()
+  const gameModel = getGameModel(game)
+  return gameModel
+}
+
+const answerQuestion = answer => {
+  const updatedGame = compose.answerQuestion(answer)
+  
+  if(updatedGame.complete){
+    
+    const questions = updatedGame.questions.map(question => ({
+      question.text,
+      question.correctAnswer
+    })
+    
+    const answers = updatedGame.answers.map(a => a.value)
+    const gameStatistics = compose.getGameStatistics(updatedGame.id)
+    const { totalCorrect, totalAnswered } = gameStatistics
+    
+    const completedGameModel = globalViewModel.completedGame(
+      updatedGame.id,
+      questions,
+      answers,
+      totalCorrect,
+      totalAnswered
+    )
+    
+    return completedGameModel
+  
+  }else{
+    const gameModel = getGameModel(updatedGame)
+    return gameModel
+  }
+}
 
 module.exports = {
-  createGame
+  createGame,
+  answerQuestion
 }
