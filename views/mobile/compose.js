d807ed737216f183fca600ad9c17c5d587112423
severnsc <severnsc@gmail.com>
2018-03-15 12:01:42 -0400

    write compose function at top of RN repo

---
+++ views/mobile/compose.js
@@ -0,0 +1,17 @@
+const globalCompose = require('../globalViewCompose')
+const testStorageAdapter = require('../../adapters/testStorageAdapter')
+const core = require('../../lib')
+
+const createGameAdapter = testStorageAdapter.createGame
+const getGameById = testStorageAdapter.getGameById
+const updateGame = testStorageAdapter.updateGame
+
+const createGame = globalCompose.createGameWithQuestions(createGameAdapter)
+const answerQuestion = core.answerQuestionInteractor(getGameById)(updateGame)
+const getGameStatistics = core.getGameStatisticsInteractor(getGameById)
+
+module.exports = {
+  createGame,
+  answerQuestion,
+  getGameStatistics
+}
