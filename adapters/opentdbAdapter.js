b2ed245134cf31a9a34b381e6139f6c372da442e
severnsc <severnsc@gmail.com>
2018-03-15 16:23:27 -0400

    Add error handling to async operations in adapters

--- adapters/opentdbAdapter.js
+++ adapters/opentdbAdapter.js
@@ -3,8 +3,8 @@ const core = require('../lib')
 
 const fetchQuestions = async () => {
   const url = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
-  const response = await fetch(url)
-  const json = await response.json()
+  const response = await fetch(url).catch(err => {throw err})
+  const json = await response.json().catch(err => {throw err})
   const results = json.results
   const questionEntities = results.map(result => {
     return core.createQuestionEntity(result.category, result.question, result.correct_answer, result.incorrect_answers)
